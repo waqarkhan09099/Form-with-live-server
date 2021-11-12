904abac0241fd42b3928377f612c587a065de3f6
Muhammad Waqar Khan <63258252+waqarkhan09099@users.noreply.github.com>
2021-11-12 22:55:57 +0500

    update

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 import 'axios'
 const form = document.getElementById('form')
-getPost()
-function getPost(){
-    axios.get('https://waqar-server-mongodb.herokuapp.com/api/users').then(responce => {
-        console.log(responce)
-        document.getElementById('data_row').innerHTML=responce.data.map((data,index)=>`
-        <tr>
-                  <th scope="row">${index===0?'1':index+1}</th>
-                  <td>${data.name}</td>
-                  <td>${data.email}</td>
-                  <td>${data.address}</td>
-                  <td>
-                    <button type="button" onclick='editUser()' class="btn btn-primary"><i class="far fa-eye"></i></button>
-                    <button type="button" onclick='deleteUser()' class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
-                  </td>
-                </tr>
-        `)
-
-    })
-
-}
 
 
 form.addEventListener('submit', (e) => {
@@ -41,6 +21,26 @@ form.addEventListener('submit', (e) => {
             A simple success alert—check it out!
          </div>`
 
+    function getPost() {
+        axios.get('https://waqar-server-mongodb.herokuapp.com/api/users').then(responce => {
+            console.log(responce)
+            document.getElementById('data_row').innerHTML = responce.data.map((data, index) => `
+                <tr>
+                          <th scope="row">${index === 0 ? '1' : index + 1}</th>
+                          <td>${data.name}</td>
+                          <td>${data.email}</td>
+                          <td>${data.address}</td>
+                          <td>
+                            <button type="button" onclick='editUser()' class="btn btn-primary"><i class="far fa-eye"></i></button>
+                            <button type="button" onclick='deleteUser()' class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                          </td>
+                        </tr>
+                `)
 
-    
+        })
+
+    }
+
+    getPost()
+
 })
